2e93267d29930369199d22e2110ace99461276c0
Justin Sienicki <justinsienicki@att.net>
2021-01-15 17:35:20 -0600

    Created model, repo interface, and controller

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,29 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Commander.Controllers
+{
+    //api/commands  -- if you change name of class this route will change as well [controler]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //Get api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem =_repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,24 @@
+﻿using Commander.Models;
+using System.Collections.Generic;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan"},
+                new Command { Id = 1, HowTo = "Google", Line = "Type in Search", Platform = "Computer"},
+                new Command { Id = 3, HowTo = "Drive", Line = "Push Gas", Platform = "Car"}
+            };
+        return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" };
+        }
+    }
+}
