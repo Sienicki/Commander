8e92c25ad2bdb7cd0ed5e10c70056e42656fe2eb
Justin Sienicki <justinsienicki@att.net>
2021-01-17 13:58:25 -0600

    Implemented Interface

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -32,7 +32,9 @@ namespace Commander
             services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
                 (Configuration.GetConnectionString("Commanderonnection")));
             services.AddControllers();
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            //services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Commander", Version = "v1" });
